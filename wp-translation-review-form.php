be0127982a24b01f0bc7d17d3f7e178dd663403e
Felipe Santos <info@felipels.com>
2021-01-19 22:25:12 +0000

    WP-CLI plugin creation

---
+++ wp-translation-review-form.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     WP Translation Review Form
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-translation-review-form
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Translation_Review_Form
+ */
+
+// Your code starts here.
